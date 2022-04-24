8a7b84c9ab42df8df860ea6979640767f0e7a331
volodin32104 <volodin32104@gmail.com>
2022-04-24 20:36:53 +0900

    Updates

---
+++ precache-manifest.6704b8d6c1dfef70266f1540548eff9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aaf3a0d7c1e10fcc4ef",
+    "url": "/volodin32104/react-app-movies/static/css/main.a9b40b28.chunk.css"
+  },
+  {
+    "revision": "9aaf3a0d7c1e10fcc4ef",
+    "url": "/volodin32104/react-app-movies/static/js/main.9aaf3a0d.chunk.js"
+  },
+  {
+    "revision": "2457350d681ed971bd0b",
+    "url": "/volodin32104/react-app-movies/static/js/1.2457350d.chunk.js"
+  },
+  {
+    "revision": "0040336180effaf9dd81",
+    "url": "/volodin32104/react-app-movies/static/js/runtime~main.00403361.js"
+  },
+  {
+    "revision": "b3c3cf1b9a452f637f46b6ac2cc58880",
+    "url": "/volodin32104/react-app-movies/index.html"
+  }
+];
